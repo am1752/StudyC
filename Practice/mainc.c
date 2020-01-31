cec7201477a850d16d867a7ec78039339981212f
am1752 <am1752@naver.com>
2020-01-31 11:21:59 +0900

    챕터 2

--- Practice/mainc.c
+++ Practice/mainc.c
@@ -5,8 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-int total(int kor, int eng, int mat) {
+int kor, eng, mat;
+int total() {
 	int sum = kor + eng + mat;
 	return sum;
 }
@@ -17,11 +17,18 @@ double average(int sum) {
 
 }
 
-
-
+void print() {
+	printf("          ====성적표====        \n");
+	printf("---------------------------------\n");
+	printf("  국어  영어  수학  총점  평균\n");
+	printf("%6d%6d%6d%6d%6.1lf\n", kor, eng, mat, total(), average(total()));
+	printf("---------------------------------\n");
+}
 void main() {
 	//type here
+	scanf_s("%d %d %d", &kor, &eng, &mat);
 
+	print();
 	
 	system("pause");
 }
