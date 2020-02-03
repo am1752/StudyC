9eb7e5ac1c86ce50b02c7fafa0a2c396e7d80010
am1752 <am1752@naver.com>
2020-02-03 17:29:41 +0900

    11

--- cc/main.c
+++ cc/main.c
@@ -23,8 +23,8 @@ struct _finddata_t fd;
 char n[1000][_MAX_FNAME];
 
 
-int check(char *name,int cnt) {
-    for (int i = 0; i <= cnt; i++) {
+int check(char* name, int cnt) {
+    for (int i = 0; i < cnt; i++) {
         if (strcmp(n[i], name) == 0) return 0;
     }
     return 1;
@@ -32,45 +32,45 @@ int check(char *name,int cnt) {
 
 
 int main(void)
-{      
+{
     int result = 1;
     int cnt = 0;
-    FILE *a;
+    FILE* a;
     char buffer[10000];
     while (1) {
         if (kbhit()) {
             if (getch() == 'a') break;
         }
 
-        
+
         if ((handle = _findfirst("d:\\documents\\*.txt", &fd)) == -1L) {
             continue;
         }
-        
+
         else {
             do {
-                char path[_MAX_PATH]="";
+                char path[_MAX_PATH] = "";
                 strcpy(path, file_path);
                 strcat(path, fd.name);
-                if (check(fd.name, cnt) ==1)
+                if (check(fd.name, cnt) == 1)
                 {
-                    
+
                     strcpy(n[cnt++], fd.name);
-                    a = fopen(path, "r+");
+                    a = fopen(path, "r");
                     if (fgets(buffer, sizeof(buffer), a) != NULL) {
                         printf("%s\n", buffer);
                     }
                     //printf("%s\n", fd.name);
-                    
+
                 }
-                
-                
+
+
             } while (_findnext(handle, &fd) != -1);
-            
-           }
-        
-        _findclose(handle);
-            
+
         }
+
+        _findclose(handle);
+
+    }
     return 0;
 }
